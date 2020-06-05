4f0b0b55a8e6cca3a98163149422a6e91225cdb2
Christian Widlund <christian.widlund@gmail.com>
2020-06-05 08:57:18 +0200

    test empty return on get_object_id and get_last_sync_id

--- tests/class-database.php
+++ tests/class-database.php
@@ -39,6 +39,15 @@ class Database_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEmpty( $db->get_object_id( 'post', $sync_id ) );
 	}
 
+	public function test_get_object_id_is_emtpy() {
+		$db = new Database;
+		$sync_id = 1;
+		$expected_object_id = 0;
+		$post_id = $db->get_object_id( 'post', $sync_id );
+		$this->assertSame( $expected_object_id, $post_id );
+		$this->assertSame( $expected_object_id, $db->get_last_sync_id() );
+	}
+
 	public function test_get_last_sync_id() {
 		$db = new Database;
 		$sync_id = $db->create( 1, 'post' );
@@ -46,5 +55,14 @@ class Database_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertSame( 1, $post_id );
 		$this->assertSame( $sync_id, $db->get_last_sync_id() );
 		$db->delete( $post_id, 'post' );
+	}
+
+	public function test_get_last_sync_id_is_emtpy() {
+		$db = new Database;
+		$sync_id = 1;
+		$expected_sync_id = 0;
+		$post_id = $db->get_object_id( 'post', $sync_id );
+		$this->assertSame( $expected_sync_id, $post_id );
+		$this->assertSame( $expected_sync_id, $db->get_last_sync_id() );
 	}
 }
